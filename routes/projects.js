89414a9819145e65d52aafb4eda66d0405e56482
Ivan Jakab <ivan.jakab@gauss.hr>
2021-05-18 22:57:34 +0200

    feat: for for creating project

--- routes/projects.js
+++ routes/projects.js
@@ -2,7 +2,10 @@ var express = require('express');
 var router = express.Router();
 const mongoose = require('mongoose')
 
-/* GET users listing. */
+router.get('/create', function(req, res, next) {
+    res.render('single-project', { title: 'Unesi novi projek' });
+});
+
 router.post('/', async function(req, res, next) {
     await mongoose.model('Project').create({
         name : 'testing',

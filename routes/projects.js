8540f6a4bc503906d0b83fe771dc90b72fd89d29
Ivan Jakab <ivan.jakab@gauss.hr>
2021-05-19 18:48:54 +0200

    feat: display projects

--- routes/projects.js
+++ routes/projects.js
@@ -2,6 +2,20 @@ var express = require('express');
 var router = express.Router();
 const mongoose = require('mongoose')
 
+router.get('/', async function(req, res, next) {
+    const data = await mongoose.model('Project').find()
+    for (const item of data) {
+        if (!item.name) item.name = 'empty'
+    }
+    res.render('list-projects', {projects: data});
+});
+
+router.get('/json', async function(req, res, next) {
+    const data = await mongoose.model('Project').find()
+    res.send(JSON.stringify(data))
+});
+
+
 router.get('/create', function(req, res, next) {
     res.render('single-project', { title: 'Unesi novi projek' });
 });
